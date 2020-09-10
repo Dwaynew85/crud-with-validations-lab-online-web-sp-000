886371d6704811457e3c6e4f3b717ee0132eb239
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-09-10 17:13:59 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,8 @@
 class Song < ApplicationRecord
   validates :title, presence: true && :year, uniqueness: true
-<<<<<<< HEAD
   validates :release_year, presence: true, numericality: { less_than_or_equal_to: Date.today.year },
     if: :released?
-=======
   validates :release_year, presence: true,
     if: :released?
   validates :release_year, inclusion: { in: Date.today.year }
->>>>>>> fd63f8f1b0c163b2cb40e0d04b5c415049d6f0d7
 end
